cd9aa8e48c7143943cbd88bef77db88fc2fc1777
Naman Arora <namanarora1022@gmail.com>
2025-11-06 19:15:10 +0530

    feat: admin guard

---
+++ apps/api-gateway/src/guards/admin.guard.ts
@@ -0,0 +1,52 @@
+import 'dotenv/config';
+import {
+    CanActivate,
+    ExecutionContext,
+    ForbiddenException,
+    Inject,
+    Injectable,
+    Logger,
+    UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { verify } from 'jsonwebtoken';
+import { AccessTokenPayload } from '@leetcode/types';
+import { MICROSERVICES, USERS } from '@leetcode/constants';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
+import { User } from '@leetcode/database';
+
+@Injectable()
+export class AdminGuard implements CanActivate {
+    constructor(@Inject(MICROSERVICES.USERS_SERVICE) private client: ClientProxy) {}
+
+    private logger = new Logger();
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const req = context.switchToHttp().getRequest() as Request;
+        const authorization = req.headers['authorization'];
+
+        if (!authorization) {
+            throw new UnauthorizedException();
+        }
+
+        try {
+            const token = authorization.split(' ')[1];
+            const payload = verify(token, process.env.ACCESS_TOKEN_SECRET!) as AccessTokenPayload;
+            req.userId = payload.userId;
+
+            const user = (await firstValueFrom(
+                this.client.send(USERS.CURRENT, { userId: payload.userId }),
+            )) as Omit<User, 'password' | 'emailVerfied' | 'tokenVersion'>;
+
+            if (!user.is_admin) {
+                throw new ForbiddenException();
+            }
+
+            return true;
+        } catch (error) {
+            this.logger.error(error);
+            throw new UnauthorizedException();
+        }
+    }
+}
