7a957556b2c3783d5c891fb95873a5fddf707f65
Naman Arora <namanarora1022@gmail.com>
2025-11-06 12:07:07 +0530

    chore: clean

--- apps/users-service/src/app.module.ts
+++ apps/users-service/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { DatabaseModule } from '@leetcode/database';
 
 @Module({
     imports: [DatabaseModule],
-    controllers: [AppController, UsersController],
-    providers: [AppService],
+    controllers: [UsersController],
+    providers: [],
 })
 export class AppModule {}
