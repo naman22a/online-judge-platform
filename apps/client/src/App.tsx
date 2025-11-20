f48d545b40d1c9e765f48adcfbd07533dfe809d2
Naman Arora <namanarora1022@gmail.com>
2025-11-20 15:23:53 +0530

    feat: dark mode

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -7,22 +7,25 @@ import Register from './pages/Register';
 import ConfirmEmail from './pages/ConfirmEmail';
 import ProblemDetails from './pages/ProblemDetails';
 import { Toaster } from 'sonner';
+import { ThemeProvider } from './components/theme-provider';
 
 const queryClient = new QueryClient();
 
 const App: React.FC = () => {
     return (
         <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/confirm/:token" element={<ConfirmEmail />} />
-                    <Route path="/problems/:slug" element={<ProblemDetails />} />
-                </Routes>
-            </BrowserRouter>
-            <Toaster position="top-center" />
+            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/confirm/:token" element={<ConfirmEmail />} />
+                        <Route path="/problems/:slug" element={<ProblemDetails />} />
+                    </Routes>
+                </BrowserRouter>
+                <Toaster position="top-center" />
+            </ThemeProvider>
         </QueryClientProvider>
     );
 };
--- apps/client/src/components/Header.tsx
+++ apps/client/src/components/Header.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import LogoutButton from './LogoutButton';
+import { ModeToggle } from './mode-toggle';
 
 const Header: React.FC = () => {
     return (
         <header className="shadow py-5 px-10 flex items-center justify-between">
             <h1 className="font-semibold text-4xl">Leetcode</h1>
-            <LogoutButton>Logout</LogoutButton>
+            <div className="flex items-center gap-5">
+                <ModeToggle />
+                <LogoutButton>Logout</LogoutButton>
+            </div>
         </header>
     );
 };
