87340d9b118b24d61680be35718c4afefdef4c55
Naman Arora <namanarora1022@gmail.com>
2025-11-21 12:26:14 +0530

    fix: try to fix server down issue

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -58,6 +58,6 @@ async function bootstrap() {
     SwaggerModule.setup('api', app, document);
 
     // STARTING THE SERVER
-    await app.listen(port);
+    await app.listen(port, '0.0.0.0');
 }
 bootstrap();
