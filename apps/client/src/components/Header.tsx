8665a6237a31833acc711a6f385154d6942fd06c
Naman Arora <namanarora1022@gmail.com>
2026-01-08 20:19:55 +0530

    refactor: Header ui ux improvement

--- apps/client/src/components/Header.tsx
+++ apps/client/src/components/Header.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import LogoutButton from './LogoutButton';
 import { ModeToggle } from './mode-toggle';
+import { Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import * as api from '@/api';
 
 const Header: React.FC = () => {
+    const meQuery = useQuery({ queryKey: ['users', 'me'], queryFn: api.users.me });
+
     return (
         <header className="shadow py-5 px-10 flex items-center justify-between">
-            <h1 className="font-semibold text-4xl">Leetcode</h1>
+            <Link to="/">
+                <h1 className="font-semibold text-4xl">Leetcode</h1>
+            </Link>
             <div className="flex items-center gap-5">
+                <h2>
+                    @<span className="font-semibold text-lg">{meQuery.data?.username}</span>
+                </h2>
                 <ModeToggle />
                 <LogoutButton>Logout</LogoutButton>
             </div>
