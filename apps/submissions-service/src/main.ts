40c0caf40e605b00e48b5a85069619d5034db8b8
Naman Arora <namanarora1022@gmail.com>
2026-03-16 10:54:47 +0530

    fix: main.ts in submissions service

---
+++ apps/submissions-service/src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Configuration } from '@leetcode/config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { startMetricsServer } from './metrics-server';
+import { register } from 'prom-client';
+
+async function bootstrap() {
+    const appContext = await NestFactory.createApplicationContext(AppModule);
+    const configService = appContext.get(ConfigService<Configuration>);
+
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+        transport: Transport.TCP,
+        options: {
+            host: '0.0.0.0',
+            // eslint-disable-next-line
+            port: configService.get('submissions_service_port'),
+        },
+    });
+    startMetricsServer(
+        register,
+        Number(String(configService.get('submissions_service_port')).replace('500', '900')),
+    );
+    await app.listen();
+}
+bootstrap();
